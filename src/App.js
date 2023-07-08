281f49fbcc94d1b0bae73c0bd14d58059bf27f00
akshaiy-14 <akshayram14@gmail.com>
2023-07-08 01:36:26 -0500

    Added error handling for incorrect API urls

--- src/App.js
+++ src/App.js
@@ -7,35 +7,57 @@ function App() {
   const [posts, setPosts] = useState([]);
   const [comments, setComments] = useState([]);
   const [users, setUsers] = useState([]);
+  const [fetchError, setFetchError] = useState(null);
   const [p,setP] = useState(false);
   const [c, setC] = useState(false);
   const [u, setU] = useState(false);
 
   const fetchPosts = async () => {
-    const response = await fetch(`https://jsonplaceholder.typicode.com/posts`);
-    const result = await response.json();
-    setPosts(result);
-    setP(true);
-    setC(false);
-    setU(false);
+    try {
+      setFetchError(null);
+      const response = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+      if (!response.ok) throw Error("Check the API source URL");
+      const result = await response.json();
+      setPosts(result);
+      setP(true);
+      setC(false);
+      setU(false);
+    }
+    catch (err) {
+      setFetchError(err.message);
+    }
   }
 
   const fetchComments = async () => {
-    const response = await fetch(`https://jsonplaceholder.typicode.com/comments`);
-    const result = await response.json();
-    setComments(result);
-    setC(true);
-    setU(false);
-    setP(false);
+    try {
+      setFetchError(null);
+      const response = await fetch(`https://jsonplaceholder.typicode.com/comments`);
+      if (!response.ok) throw Error("Check the API source URL");
+      const result = await response.json();
+      setComments(result);
+      setC(true);
+      setU(false);
+      setP(false);
+    }
+    catch (err) {
+      setFetchError(err.message);
+    }
   }
 
   const fetchUsers = async () => {
-    const response = await fetch(`https://jsonplaceholder.typicode.com/users`);
-    const result = await response.json();
-    setUsers(result);
-    setU(true);
-    setP(false);
-    setC(false);
+    try {
+      setFetchError(null);
+      const response = await fetch(`https://jsonplaceholder.typicode.com/users`);
+      if (!response.ok) throw Error("Check the API source URL");
+      const result = await response.json();
+      setUsers(result);
+      setU(true);
+      setP(false);
+      setC(false);
+    }
+    catch (err) {
+      setFetchError(err.message);
+    }
   }
 
   return (
@@ -47,9 +69,11 @@ function App() {
       <button onClick={fetchComments}> Fetch Comments </button>
       <button onClick={fetchUsers}> Fetch Users </button>
     </div>
-    <div>
+    {fetchError && <h2> ERROR: {fetchError} </h2>}
+    {!fetchError && <div>
       <ContentList p={p} c={c} u={u} posts={posts} comments={comments} users={users} />
-    </div>
+    </div> }
+    
     </>
   );
 }
